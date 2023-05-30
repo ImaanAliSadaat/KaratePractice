af7799ff2ce73267217d28e1cc73ec93568e9d2d
test@test.com <test@test.com>
2023-05-29 20:05:27 -0700

    forth practice

---
+++ src/Karate_Practice/forthClass.java
@@ -0,0 +1,11 @@
+package Karate_Practice;
+
+public class forthClass {
+public static void main(String[] args) {
+int sum1 = 25;
+int sum2 = 50;
+int result = sum1 + sum2;
+
+	System.out.println(result);
+}
+}
